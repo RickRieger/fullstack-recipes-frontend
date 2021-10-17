e54fc13822adb62a2f0105b64aefdd7ed28a05db
Rick Rieger <rickrieger36251@gmail.com>
2021-10-17 17:51:51 -0400

    changed back

--- src/components/utils/Axios.js
+++ src/components/utils/Axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 const Axios = axios.create({
   baseURL:
-    process.env.REACT_APP_AXIOS === 'development'
-      ? 'http://localhost:3001'
-      : '/api',
+    process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '/api',
   timeout: 50000,
 });
 export default Axios;
